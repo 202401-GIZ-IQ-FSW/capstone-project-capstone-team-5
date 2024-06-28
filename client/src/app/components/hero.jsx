9a435acc68719f9773732313c2bfd4e66796468b
Sarah Azad <aras.azad3@gmail.com>
2024-06-28 14:59:55 +0300

    feat: add job categories components

--- client/src/app/components/hero.jsx
+++ client/src/app/components/hero.jsx
@@ -26,11 +26,11 @@ export default function Hero() {
               <span className="sr-only">Your Company</span>
             </a>
             <Image
-            src="/logo.png"
-            width={160}
-            height={120}
-            alt="Picture of the author"
-          />
+              src="/logo.png"
+              width={160}
+              height={120}
+              alt="Picture of the author"
+            />
           </div>
           <div className="flex lg:hidden">
             <button
@@ -114,19 +114,7 @@ export default function Hero() {
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          {/* <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80fffd] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          /> */}
-        </div>
+      <div className="relative px-6 pt-14 lg:px-8">
         <div className="flex justify-center pt-12">
           <Image
             src="/heroimage.png"
@@ -145,24 +133,12 @@ export default function Hero() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <input
-                className="h-10  lg:w-[800px] bg-white px-4 py-6 rounded-md placeholder:text-black border-black border-2 outline-[#7ad6ea]"
+                className="h-10  placeholder:text-sm lg:placeholder:text-lg w-[240px] lg:w-[800px] bg-white px-4 py-6 rounded-md placeholder:text-black border-black border-2  outline-[#7ad6ea]"
                 type="text"
                 placeholder="What job are you seeking?"
               />
             </div>
           </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          {/* <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#80e6ff] to-[#3e1dc4] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          /> */}
         </div>
       </div>
     </div>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,45 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import CategoryCard from "./components/category-card";
+import { Cards } from "./constants/cards";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#f5f3f3]">
       <Hero />
+      <div className="mt-12 mb-10 text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Popular Job Categories
+        </h1>
+      </div>
+      <div className="flex flex-col">
+        {" "}
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-4 p-8 mx-auto ">
+          {Cards.map((card) => {
+            return (
+              <CategoryCard
+                name={card.name}
+                imageUrl={card.imageUrl}
+                listing={card.listing}
+              />
+            );
+          })}
+          <div className=" flex flex-col w-[260px] col-span-1 divide-gray-200 rounded-lg bg-white shadow p-6 justify-between border-2 border-black  hover:transform hover:-translate-y-2 transition hover:shadow-xl hover:cursor-pointer group">
+            <div className="mt-12">
+              <h3 className="truncate text-2xl font-semibold text-gray-900">
+                +10K Jobs
+              </h3>
+              <p className="mt-1 truncate text-sm text-gray-500">Apply Now!</p>
+            </div>
+            <div className="flex justify-between items-end">
+              <button className="bg-[#80e6ff] rounded-md group-hover:bg-[#f1ff52] border-black border-2 transition-all font-medium px-6 py-2 h-10 items-center leading-3">
+                Explore
+              </button>{" "}
+              <img className="h-16 w-16 " src="./job-search.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
