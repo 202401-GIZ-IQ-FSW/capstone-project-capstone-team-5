b7169edca5e68bb1eedf94ec3a2123495bd7311b
Sarah Azad <aras.azad3@gmail.com>
2024-06-30 22:57:25 +0300

    feat: sign up page

--- client/src/app/sign-up/page.js
+++ client/src/app/sign-up/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import SignIn from "@/components/Registration/sign-in";
 import Nav from "@/components/Common/navbar";
+import SignUp from "@/components/Registration/sign-up";
 
 export default function Browse() {
   return (
     <main className="bg-[#f5f3f3] h-screen">
       <Nav />
-      <SignIn />
+      <SignUp />
     </main>
   );
 }
